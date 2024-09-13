a8872021dd6ca4e6884f53b0025f7e085e55d92d
Daniel Blancas <edblancas@gmail.com>
2024-09-13 00:20:34 -0600

    add triplet sum to zero

---
+++ two_pointers/4_triplet_sum_to_zero.py
@@ -0,0 +1,38 @@
+# medium
+# Given an array of unsorted numbers, find all unique triplets in it that add up to zero.
+
+def search_triplets(arr):
+    arr.sort()
+    triplets = []
+    for i in range(len(arr)):
+        if i > 0 and arr[i - 1] == arr[i]:
+            continue
+        search_sum(arr, -arr[i], i+1, triplets)
+    return triplets
+
+def search_sum(arr, target_sum, lo, triplets):
+    hi = len(arr) - 1
+    while lo < hi:
+        curr_sum = arr[lo] + arr[hi]
+        if curr_sum == target_sum:
+            triplets.append([-target_sum, arr[lo], arr[hi]])
+            lo += 1
+            hi -= 1
+            while lo < hi and arr[lo-1] == arr[lo]:
+                lo += 1
+            while lo < hi and arr[hi+1] == arr[hi]:
+                hi -= 1
+        elif curr_sum < target_sum:
+            lo += 1
+        else:
+            hi -= 1
+
+import unittest
+
+class TestSearchTriplets(unittest.TestCase):
+    def test_search_triplets(self):
+        self.assertEqual(search_triplets([-3, 0, 1, 2, -1, 1, -2]), [[-3, 1, 2], [-2, 0, 2], [-2, 1, 1], [-1, 0, 1]])
+        self.assertEqual(search_triplets([-5, 2, -1, -2, 3]), [[-5, 2, 3], [-2, -1, 3]])
+
+if __name__ == '__main__':
+    unittest.main()
