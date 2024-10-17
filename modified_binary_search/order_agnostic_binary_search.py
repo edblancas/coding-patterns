5b2f05b0a2efc48da3d3f710a855201379b8193e
Daniel Blancas <edblancas@gmail.com>
2024-10-17 09:25:16 -0600

    add another pattern

---
+++ modified_binary_search/order_agnostic_binary_search.py
@@ -0,0 +1,40 @@
+def binary_search(arr, key):
+    isAsc = True
+    if arr[0] > arr[-1]:
+        isAsc = False
+
+    lo, hi = 0, len(arr) - 1
+    while lo <= hi:
+        m = (lo + hi) // 2
+        if arr[m] == key:
+            return m
+        elif key > arr[m]:
+            if not isAsc:
+                hi = m - 1
+            else:
+                lo = m + 1
+        else:
+            if not isAsc:
+                lo = m + 1
+            else:
+                hi = m - 1
+
+    return -1
+
+
+import unittest
+
+class TestBinarySearch(unittest.TestCase):
+    def test_binary_search(self):
+        self.assertEqual(binary_search([4, 6, 10], 10), 2)
+        self.assertEqual(binary_search([1, 2, 3, 4, 5, 6, 7], 5), 4)
+        self.assertEqual(binary_search([10, 6, 4], 10), 0)
+        self.assertEqual(binary_search([10, 6, 4], 4), 2)
+        self.assertEqual(binary_search([1, 2, 3, 4, 5, 6, 7], 99), -1)
+
+def main():
+    test = TestBinarySearch()
+    test.test_binary_search()
+
+
+main()
